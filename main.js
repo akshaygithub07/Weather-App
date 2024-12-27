ab86e71d144ef68b9d42bd4240bbec4c01ec47a9
Fire <akshaysunilkumar2004@gmail.com>
2024-12-27 20:17:57 +0530

    certain api issues fixed

---
+++ main.js
@@ -0,0 +1,58 @@
+import apiKey from './config.js';
+
+
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.main.wind.speed + "km/h";
+    
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";
+
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png";
+
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";
+
+    }
+   
+   
+
+const weatherElement = document.querySelector(".weather");
+if (weatherElement) {
+    weatherElement.style.display = "block";
+    console.log("Weather element displayed");
+} else {
+    console.log("Weather element not found");
+}
+
+    
+}
+
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value);
+
+})
